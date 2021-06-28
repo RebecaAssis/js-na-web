8637e508dc1c8296233dad20b0b8f3afe6318a41
Rebeca Assis <rebeca.oassis@hotmail.com>
2021-06-28 18:37:47 -0300

    Realizando a requisição com fetch.

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -18,32 +18,11 @@ const tabela = document.querySelector("[data-tabela]");
 
 const listaClientes = () => {
 
-    const promise = new Promise((resolve, reject) => {
-        const http = new XMLHttpRequest();
-
-        // 1 - abrindo a comunicação
-        // 2 - enviando a comunicação
-        // 3 - ao carrgar a página
-        // 4 - pegando a resposta do servidor
-        // 5 - lembre-se que o http nos retorna um TEXTO. Teremos que transformar essa resposta em um objeto js para consguirmos fazer o forEach (percorrer o objeto e pegar os dados)
-
-        http.open("GET", "http://localhost:3000/profile");
-        http.onload = () => {
-
-            if(http.status >= 400) {
-
-                reject(JSON.parse(http.response));
-
-            } else {
-
-                resolve(JSON.parse(http.response));
-            }
-
-        }
-        http.send();
+    return fetch(`http://localhost:3000/profile`)
+    .then(resposta => {
+        return resposta.json();
     });
     
-    return promise;
 };
 
 listaClientes()
