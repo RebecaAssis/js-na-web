ad9e71bdfb3298c842d872df2c06c10efd3757b4
Rebeca Assis <rebeca.oassis@hotmail.com>
2021-07-01 15:42:08 -0300

    Uso do fetch para editar e excluir um cliente.

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -1,21 +1,3 @@
-const criaNovaLinha = (nome, email) => {
-    const linhaNovoCliente = document.createElement("tr");
-    const conteudo = `
-    <td class="td" data-td>${nome}</td>
-    <td>${email}</td>
-    <td>
-        <ul class="tabela__botoes-controle">
-            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-        </ul>
-    </td>
-    `
-    linhaNovoCliente.innerHTML = conteudo;
-    return linhaNovoCliente;
-}
-
-const tabela = document.querySelector("[data-tabela]");
-
 const listaClientes = () => {
 
     return fetch(`http://localhost:3000/profile`)
@@ -25,9 +7,58 @@ const listaClientes = () => {
     
 };
 
-listaClientes()
-.then(data => {
-    data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
+const criaCliente = (nome, email) => {
+
+    return fetch(`http://localhost:3000/profile`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    .then(resposta => {
+        return resposta.body
     });
-});
+
+};
+
+const removeCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: "DELETE"
+    })
+};
+
+const detalhaCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`)
+    .then(resposta => {
+        return resposta.json();
+    });
+};
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    .then(resposta => {
+        return resposta.json()
+    })
+};
+
+export const clienteService = {
+    listaClientes,
+    criaCliente,
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
+};
+
